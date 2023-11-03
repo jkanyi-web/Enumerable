6ec24373fa98cf9952b2c97eee2b956e9f25c87e
bhushan354 <deshmukhumesh354@gmail.com>
2023-11-03 08:35:33 +0530

    Rubocop errors fix and get desired output

--- main.rb
+++ main.rb
@@ -1,18 +1,14 @@
 require_relative 'my_list'
-require_relative 'my_enumerable'
 
-# Create our list
 list = MyList.new(1, 2, 3, 4)
-puts list.inspect
 
-# Test for #all?
-puts list.all? { |e| e < 5 }
-puts list.all? { |e| e > 5 }
+# Test #all?
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
-# Test fr #any?
-puts list.any? { |e| e == 2 }
-puts list.any? { |e| e == 5 }
+# Test #any?
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
-# filter
-filtered = list.filter(&:even?)
-puts filtered.inspect
+# Test #filter
+puts(list.filter(&:even?))
